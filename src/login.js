bfd4b022d29032e1e6ef487b2806b419b169cd49
Jean de Campredon <jean.decampredon@gmail.com>
2019-05-15 16:40:57 +0200

    chore: update login form

--- src/login.js
+++ src/login.js
@@ -1,28 +1,21 @@
 require('dotenv').config();
 
 export default async page => {
-  await page.goto('https://www.linkedin.com/');
+  await page.goto('https://www.linkedin.com/uas/login?trk=guest_homepage-basic_nav-header-signin');
 
-  await page.waitForSelector(
-    '.global-wrapper > .header > .wrapper > .login-form > #login-password',
-  );
-  await page.type(
-    '.global-wrapper > .header > .wrapper > .login-form > #login-email',
-    process.env.USERNAME,
-    {delay: 50}
-  );
+  const inputSelector =
+    'html > body > #app__container > main > div > form > div:nth-child(7) > #username';
+  await page.waitForSelector(inputSelector);
+  await page.type(inputSelector, process.env.USERNAME, { delay: 50 });
 
-  await page.waitForSelector(
-    '.global-wrapper > .header > .wrapper > .login-form > #login-password',
-  );
-  await page.type(
-    '.global-wrapper > .header > .wrapper > .login-form > #login-password',
-    process.env.PASSWORD,
-    {delay: 50}
-  );
+  const passwordSelector =
+    'html > body > #app__container > main > div > form > div:nth-child(25) > #password';
+  await page.waitForSelector(passwordSelector);
+  await page.type(passwordSelector, process.env.PASSWORD, { delay: 50 });
 
-  await page.waitForSelector('.global-wrapper > .header > .wrapper > .login-form > #login-submit');
-  await page.click('.global-wrapper > .header > .wrapper > .login-form > #login-submit');
+  await page.click(
+    'html > body > #app__container > main > div > form > div:nth-child(26) > button',
+  );
 
   await page.waitForNavigation();
 
