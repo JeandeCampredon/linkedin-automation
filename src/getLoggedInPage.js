26a44ac1af72ff7e10c40eb1274383ec365bffab
Jean de Campredon <jean.decampredon@gmail.com>
2019-05-16 15:30:16 +0200

    chore: add env options for windows wsl

--- src/getLoggedInPage.js
+++ src/getLoggedInPage.js
@@ -1,11 +1,19 @@
 import puppeteer from 'puppeteer-core';
+import rimraf from 'rimraf';
 import login from './login';
 import conf from '../conf.json';
 
 export default async function getLoggedInPage(launchConf = {}) {
-  const browser = await puppeteer.launch({
+  const envConf = {
     headless: false,
     executablePath: process.env.EXE_PATH,
+  }
+  if (process.env.USER_DATA_DIR) {
+    envConf.userDataDir = process.env.USER_DATA_DIR;
+    rimraf(process.env.USER_DATA_DIR_WSL)
+  }
+  const browser = await puppeteer.launch({
+    ...envConf,
     ...launchConf,
   });
   const page = await browser.newPage();
